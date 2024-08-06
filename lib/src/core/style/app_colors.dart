f649774bf77a08335141dbabbbee0462c1439bf2
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-08-06 13:51:04 +0500

    Xurshid app colors add

--- lib/src/core/style/app_colors.dart
+++ lib/src/core/style/app_colors.dart
@@ -1,15 +1,18 @@
 import 'dart:ui';
 
-final class AppColors{
-
-
-  static const Color cFF5C00 = Color(0xffFF5C00);
-
-  static const Color primaryColor = Color(0xffffffff);
-
-  static const Color secondaryColor = Color(0xffA8AFB9);
-  static const Color darkPrimaryColor = Color(0xff040101);
-
-
-
+final class AppColors {
+  //dicoveriy page top colors
+  static const Color lF83B01 = Color(0xffF83B01); //eng kop ishlatilgan rang ekan
+  static const Color lF96234 = Color(0xffF96234);
+  static const Color lFED8CC = Color(0xffFED8CC);
+  //discover page MAIN colors
+  static const Color l0B0C17 = Color(0xff0B0C17);
+  static const Color lB6B7C0 = Color(0xffB6B7C0);
+  //discover page bottom colors
+  static const Color lFED8800 = Color(0xffFED880);
+  static const Color l494C61 = Color(0xff494C61); //top icon va kichik sizeli taxtlarning rangi
+  //restarant page main card colors
+  static const Color lFFE7CC = Color(0xffFFE7CC);
+  static const Color lEDEDEF = Color(0xffEDEDEF); //textFild colors
+  static const Color l767989 = Color(0xff767989); //textFild icon colors
 }
