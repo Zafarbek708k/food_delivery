a1f8ed519d568679505581842ce206b2f3d8069f
Asadbek27Code <arashi27asadbek@gmail.com>
2024-08-07 17:12:20 +0500

    commit by Asadbek const requared

--- lib/src/core/widgets/app_material_context.dart
+++ lib/src/core/widgets/app_material_context.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/src/feature/auth/pages/verificationcode_page.dart';
 import 'package:food_delivery/src/feature/restaurant/page/delivery_map_page.dart';
 import '../../feature/settings/inherited_locale_notifier.dart';
 import '../../feature/settings/inherited_theme_notifier.dart';
@@ -21,7 +22,7 @@ class AppMaterialContext extends StatelessWidget {
             builder: (context) {
               return MaterialApp(
                 // home: const LoginPage(),
-                home: const MapDeliveryPage(),
+                home: const VerificationcodePage(),
                 debugShowCheckedModeBanner: false,
                 theme: InheritedThemeNotifier.maybeOf(context)?.theme,
                 locale: InheritedLocalNotifier.maybeOf(context)?.appLocal,
