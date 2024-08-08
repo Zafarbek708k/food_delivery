5d4d94e00374aa6ca4b3a1c1b4fef9dbe2799aee
Akmal123 <akmalahmadjonov798@gmail.com>
2024-08-08 15:11:53 +0000

    akmal pull

--- lib/src/feature/restaurant/page/delivery_map_page.dart
+++ lib/src/feature/restaurant/page/delivery_map_page.dart
@@ -1,16 +1,155 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
+import '../../../core/style/app_colors.dart';
 
-class MapDeliveryPage extends StatelessWidget {
+class MapDeliveryPage extends StatefulWidget {
   const MapDeliveryPage({super.key});
+
+  @override
+  State<MapDeliveryPage> createState() => _MapDeliveryPageState();
+}
+
+class _MapDeliveryPageState extends State<MapDeliveryPage> {
+  late Position myPosition;
+  late YandexMapController yandexMapController;
+  List<MapObject> mapObjectList = [];
+  bool isLoading = false;
+
+  void onMapCreated(YandexMapController controller) {
+    yandexMapController = controller;
+    yandexMapController.moveCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: Point(latitude: myPosition.latitude, longitude: myPosition.longitude),
+          zoom: 13,
+          tilt: 900,
+          azimuth: 180,
+        ),
+      ),
+    );
+
+
+    makeRoute();
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    myPosition = await Geolocator.getCurrentPosition();
+    isLoading = true;
+    setState(() {});
+    return myPosition;
+  }
+
+  Future<void> makeRoute() async {
+    var route = YandexDriving.requestRoutes(
+      drivingOptions: const DrivingOptions(
+        routesCount: 3,
+        // avoidPoorConditions: true,
+      ),
+      points: [
+        /// Yo'lni boshlanish nuqtasi
+        RequestPoint(
+            point: Point(
+              latitude:  myPosition.latitude,
+              longitude: myPosition.longitude,
+            ),
+            requestPointType: RequestPointType.wayPoint),
+
+        /// Yo'lni tugash nuqtasi
+        RequestPoint(point: Point(latitude: myPosition.latitude+0.0014, longitude: myPosition.longitude+0.0014), requestPointType: RequestPointType.wayPoint),
+      ],
+    );
+
+
+    var result = await route.result;
+    log(result.routes.toString());
+
+    if(result.routes!.isNotEmpty){
+      for (var element in result.routes ?? []) {
+        mapObjectList.add(
+          PolylineMapObject(
+            mapId: MapObjectId("route_${124.toString()}"),
+            polyline: Polyline(
+              points: element.geometry,
+            ),
+            strokeColor: AppColors.lF83B01    ,
+            strokeWidth: 4,
+          ),
+        );
+        // mapObjectList.removeRange(1, mapObjectList.length-1);
+      }
+    }
+    setState(() {});
+  }
+  void findMe() {
+    yandexMapController.moveCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: Point(
+              latitude: myPosition.latitude,
+              longitude: myPosition.longitude,
+            ),
+            zoom: 18,
+            tilt: 50,
+            azimuth: 180,
+          ),
+        ),
+        animation: const MapAnimation(type: MapAnimationType.smooth, duration: 2));
+
+    yandexMapController.moveCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: Point(latitude: myPosition.latitude, longitude: myPosition.longitude),
+            zoom: 19,
+            tilt: 90,
+            azimuth: 180,
+          ),
+        ),
+        animation: const MapAnimation(type: MapAnimationType.smooth, duration: 4));
+  }
+
+  @override
+  void initState() {
+    _determinePosition().then((_){
+      findMe();
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: isLoading ? Stack(
         children: [
-          const YandexMap(),
+          YandexMap(
+            nightModeEnabled: true,
+            mode2DEnabled: true,
+            onMapCreated: onMapCreated,
+            mapObjects: mapObjectList,
+          ),
 
           Positioned(
             bottom: 0,
@@ -96,7 +235,7 @@ class MapDeliveryPage extends StatelessWidget {
             ),
           )
         ],
-      ),
+      ) : const Center(child: CircularProgressIndicator(),),
     );
   }
 }
