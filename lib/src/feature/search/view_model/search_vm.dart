310303b094db860aa7a22c1c068141a9b06601dc
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-08-09 15:24:11 +0500

    Xurshid searchController, search page restaran page

---
+++ lib/src/feature/search/view_model/search_vm.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+final searchVM = ChangeNotifierProvider((ref) => SearchVm());
+
+class SearchVm with ChangeNotifier {
+  TextEditingController searchController = TextEditingController();
+}
