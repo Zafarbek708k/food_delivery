7a22ac667822ab4b4de9071b6e1bc99cf5630ade
ahmad <tdax962@gmail.com>
2024-08-10 18:41:33 +0500

    Shohahatam 15 - commit  profilega yana yangilik  stylef

--- lib/src/feature/profile/view/pages/profile_page.dart
+++ lib/src/feature/profile/view/pages/profile_page.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:flutter_screenutil/flutter_screenutil.dart";
 import "package:flutter_svg/svg.dart";
 import "package:food_delivery/src/core/style/app_colors.dart";
 import "../../../../core/style/text_style.dart";
