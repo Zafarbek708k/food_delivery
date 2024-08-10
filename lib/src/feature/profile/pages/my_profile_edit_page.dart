0798f39f713a6d6ccaae36a75b36b0cd1fc62131
ahmad <tdax962@gmail.com>
2024-08-10 16:02:52 +0500

    Shohahatam 12 - commit  profilega yana yangilik qo'shildi

--- lib/src/feature/profile/pages/my_profile_edit_page.dart
+++ lib/src/feature/profile/pages/my_profile_edit_page.dart
@@ -10,33 +10,71 @@ class MyProfileEditPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final TextEditingController _namecontroller = TextEditingController();
     final name = ref.watch(nameProvider);
 
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      floatingActionButton: MaterialButton(
+        onPressed: () {
+          final newName = _namecontroller.text.trim();
+          if (newName.isNotEmpty) {
+            ref.read(nameProvider.notifier).updateName(newName);
+          }
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(300),
+        ),
+        color: Colors.greenAccent,
+        minWidth: MediaQuery.of(context).size.width * 0.9,
+        height: 60,
+        child: const Text(
+          "Save",
+          style: TextStyle(
+            color: Colors.white60,
+            fontSize: 18,
+          ),
+        ),
+      ),
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-       backgroundColor: Colors.white,
+        backgroundColor: Colors.white,
         actions: [
           PopupMenuButton<String>(
             icon: const Icon(Icons.more_vert),
             onSelected: (String value) async {
-              // Handle menu actions here
+              if (value == 'edit') {
+                await ref.read(avatarProvider.notifier).updateAvatar();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      backgroundColor: Colors.green,
+                      content: Text('Avatar updated')),
+
+                );
+              } else if (value == 'delete') {
+                await ref.read(avatarProvider.notifier).deleteAvatar();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text('Avatar deleted')),
+
+                );
+              }
             },
             itemBuilder: (BuildContext context) => [
-              const PopupMenuItem<String>(
+              PopupMenuItem<String>(
                 value: 'edit',
                 child: Row(
-                  children: [
+                  children: const [
                     Icon(Icons.edit, color: Colors.orange),
                     SizedBox(width: 8),
                     Text('Edit Avatar'),
                   ],
                 ),
               ),
-              const PopupMenuItem<String>(
+              PopupMenuItem<String>(
                 value: 'delete',
                 child: Row(
-                  children: [
+                  children: const [
                     Icon(Icons.delete, color: Colors.red),
                     SizedBox(width: 8),
                     Text('Delete Avatar'),
@@ -47,19 +85,15 @@ class MyProfileEditPage extends ConsumerWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: 18.0),
-        child: Column(
 
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // SizedBox(
-            //   height: MediaQuery.of(context).size.height * 0.02,
-            // ),
             AvatarWidget(),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
+              height: MediaQuery.of(context).size.height * 0.04,
             ),
             Text(
               name,
@@ -69,13 +103,28 @@ class MyProfileEditPage extends ConsumerWidget {
                 fontFamily: 'Rubik',
               ),
             ),
-
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
+              height: MediaQuery.of(context).size.height * 0.07,
             ),
-            EditNameWidget(),
-
-
+            TextField(
+              cursorColor: Colors.white60,
+              controller: _namecontroller,
+              decoration: InputDecoration(
+                hintText: "Your name",
+                hintStyle: const TextStyle(
+                  color: Colors.white60,
+                ),
+                filled: true,
+                fillColor: Colors.greenAccent,
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            Spacer(),
           ],
         ),
       ),
@@ -83,12 +132,9 @@ class MyProfileEditPage extends ConsumerWidget {
   }
 }
 
-
 final nameProvider = StateNotifierProvider<NameNotifier, String>((ref) {
   return NameNotifier(NameStorage());
 });
-
-
 
 class NameStorage {
   static const String _keyName = 'userName';
@@ -103,9 +149,6 @@ class NameStorage {
     await prefs.setString(_keyName, name);
   }
 }
-
-
-
 
 class NameNotifier extends StateNotifier<String> {
   final NameStorage _nameStorage;
@@ -123,4 +166,3 @@ class NameNotifier extends StateNotifier<String> {
     await _nameStorage.saveName(newName);
   }
 }
-
--- lib/src/feature/profile/widgets/edit_name_widget.dart
+++ lib/src/feature/profile/widgets/edit_name_widget.dart
@@ -5,62 +5,23 @@ import "../pages/my_profile_edit_page.dart";
 class EditNameWidget extends ConsumerWidget {
   EditNameWidget({Key? key}) : super(key: key);
 
-  final TextEditingController _namecontroller = TextEditingController();
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      children: [
-       TextField(
-          cursorColor: Colors.white60,
-          controller: _namecontroller,
-          decoration: InputDecoration(
-            hintText: "Your name",
-            hintStyle: const TextStyle(
-              color: Colors.white60,
-            ),
 
+    return Scaffold(
 
-            filled: true,
-            fillColor: Colors.greenAccent,
-            border: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-              borderSide: BorderSide.none,
-            ),
+      body: Column(
+        children: [
+
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
           ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.2,
-        ),
-        MaterialButton(
 
 
-          onPressed: () {
-
-            final newName = _namecontroller.text.trim();
-            if (newName.isNotEmpty) {
-              ref.read(nameProvider.notifier).updateName(newName);
-            }
-
-          },
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(300),
-          ),
-          color:Colors.greenAccent,
-          minWidth: double.infinity,
-          height: 60,
-          child: const Text(
-            "Save",
-            style: TextStyle(
-              color: Colors.white60,
-              fontSize: 18,
-            ),
-          ),
-        ),
-
-      ],
+        ],
+      ),
     );
   }
 }
