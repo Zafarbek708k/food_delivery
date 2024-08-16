c218e3fd635087f66120d82552fcb61f3ad984c8
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-08-16 16:58:38 +0500

    Xurshid add

--- lib/src/feature/home/view/widgets/welcome_page_info_widget.dart
+++ lib/src/feature/home/view/widgets/welcome_page_info_widget.dart
@@ -23,10 +23,19 @@ class InfoWelcomePageWidgets extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 16.h),
-            Text(context.localized.welcome, style: context.theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold, fontSize: 24)),
-            Text(context.localized.fastFood, style: context.theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w600, color: Colors.deepOrange)),
-            SizedBox(height: 16.h),
+            SizedBox(height: 35.h),
+            Text(
+              context.localized.welcome,
+              style: context.theme.textTheme.bodyLarge
+                  ?.copyWith(fontWeight: FontWeight.bold, fontSize: 28, color: Colors.yellow),
+            ),
+            SizedBox(height: 10.h),
+            Text(
+              context.localized.fastFood,
+              style: context.theme.textTheme.bodyLarge
+                  ?.copyWith(fontWeight: FontWeight.w600, fontSize: 24, color: Colors.yellow),
+            ),
+            SizedBox(height: 20.h),
             Text(
               context.localized.welcomeLastText,
               textAlign: TextAlign.center,
@@ -43,7 +52,8 @@ class InfoWelcomePageWidgets extends StatelessWidget {
               color: Colors.black,
               child: Text(
                 context.localized.startWithEmail,
-                style: context.theme.textTheme.bodyLarge?.copyWith(color: Colors.white, fontWeight: FontWeight.w600),
+                style: context.theme.textTheme.bodyLarge
+                    ?.copyWith(color: Colors.white, fontWeight: FontWeight.w600),
               ),
             ),
             SizedBox(height: 16.h),
@@ -52,7 +62,10 @@ class InfoWelcomePageWidgets extends StatelessWidget {
                 const Expanded(child: Divider(color: Colors.white)),
                 Padding(
                   padding: REdgeInsets.symmetric(horizontal: 18),
-                  child: Text(context.localized.signInWith, style: context.theme.textTheme.bodyLarge?.copyWith(color: Colors.white)),
+                  child: Text(
+                    context.localized.signInWith,
+                    style: context.theme.textTheme.bodyLarge?.copyWith(color: Colors.white),
+                  ),
                 ),
                 const Expanded(child: Divider(color: Colors.white)),
               ],
@@ -91,7 +104,8 @@ class InfoWelcomePageWidgets extends StatelessWidget {
               children: [
                 Text(
                   context.localized.alreadyHaveAnAccount,
-                  style: context.theme.textTheme.bodyLarge?.copyWith(color: Colors.black, fontWeight: FontWeight.w600),
+                  style: context.theme.textTheme.bodyLarge
+                      ?.copyWith(color: Colors.black, fontWeight: FontWeight.w600),
                 ),
                 TextButton(
                   onPressed: signInButton,
