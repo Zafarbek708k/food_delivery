78ad854b60a1929a1bb4235435e7bf9a565accb7
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-08-19 16:33:33 +0500

    Xurshid base url add restaurt api add

--- lib/src/core/server/api/api_constants.dart
+++ lib/src/core/server/api/api_constants.dart
@@ -5,29 +5,20 @@ final class ApiConst {
   static const Duration sendTimeout = Duration(minutes: 1);
   static const Duration receiveTimeout = Duration(minutes: 1);
 
-  static const String baseUrl = "http://185.74.5.104:7090";
+  static const String baseUrl = "http://45.138.158.240:8080";
   static const String version = "/api/v1";
 
-  // static const String sendSms = "$version/cabinet/sms/send";
-  static const String apiLogin = "$version/auth/login";
-  static const String apiRegister = "$version/auth/register";
-  static const String apiCheckEmail = "$version/auth/check/email";
+  // auth uchun
 
-  // get all projects list
-  static const String apiGetAllProjects = "$version/mobile/project";
-
-  // additional
-  static const String apiFAQ = "$version/mobile/faq";
-  static const String apiGetHelpCredentials = "$version/mobile/company-info";
-  static const String apiGetPrivacyPolicy = "$version/mobile/doc";
-  static const String apiVerifyUser = "$version/mobile/identification/verificate";
-  static const String apiUserMe = "$version/user/me";
+  //restaurant uchun
+  static const String apiRestaurants = "$version/product/save";
 }
 
 final class ApiParams {
   const ApiParams._();
 
-  static Map<String, dynamic> cabinetSmsCheckParams({required String phone, required String code}) => <String, dynamic>{
+  static Map<String, dynamic> cabinetSmsCheckParams({required String phone, required String code}) =>
+      <String, dynamic>{
         "phone": phone,
         "code": code,
       };
