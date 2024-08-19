d4c96b39c4b8066be4748bf2b4d83e5f28319e81
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-08-19 13:25:23 +0500

    restaurant vm add

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -1,11 +1,16 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:food_delivery/src/feature/auth/vm/auth_vm.dart";
 import "package:food_delivery/src/feature/restaurant/view_model/map_vm.dart";
+import "package:food_delivery/src/feature/restaurant/view_model/restaurant_vm.dart";
 
 final mapVM = ChangeNotifierProvider<MapVm>(
-    (ref) => MapVm(),
+  (ref) => MapVm(),
 );
 
 final authVm = ChangeNotifierProvider<AuthVm>(
-    (ref) => AuthVm(),
+  (ref) => AuthVm(),
+);
+
+final restaurantVM = ChangeNotifierProvider<RestaurantVM>(
+  (ref) => RestaurantVM(),
 );
